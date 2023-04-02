4c342d925f9adee7352c476e26fd580ca0794d9f
demeviktor02 <114135525+demeviktor02@users.noreply.github.com>
2023-04-02 18:31:34 +0200

    Client Register gomb, label, textbox átalakítás

--- Client/Stock_analysis_client/Stock_analysis_client/Properties/Resources.Designer.cs
+++ Client/Stock_analysis_client/Stock_analysis_client/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace Stock_analysis_client.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap GoldenStock {
+            get {
+                object obj = ResourceManager.GetObject("GoldenStock", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
--- Client/Stock_analysis_client/Stock_analysis_client/Register.Designer.cs
+++ Client/Stock_analysis_client/Stock_analysis_client/Register.Designer.cs
@@ -39,13 +39,16 @@
             this.Passwordlbl = new System.Windows.Forms.Label();
             this.PasswordAgainlbl = new System.Windows.Forms.Label();
             this.SignUpButtonlbl = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // SingUplbl
             // 
             this.SingUplbl.AutoSize = true;
+            this.SingUplbl.BackColor = System.Drawing.Color.Transparent;
             this.SingUplbl.Font = new System.Drawing.Font("Arial", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.SingUplbl.Location = new System.Drawing.Point(208, 31);
+            this.SingUplbl.Location = new System.Drawing.Point(310, 9);
             this.SingUplbl.Name = "SingUplbl";
             this.SingUplbl.Size = new System.Drawing.Size(160, 45);
             this.SingUplbl.TabIndex = 0;
@@ -53,46 +56,55 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
             this.button1.Location = new System.Drawing.Point(545, 381);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 40);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(137, 212);
+            this.textBox1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(178, 207);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(137, 260);
+            this.textBox2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.Location = new System.Drawing.Point(178, 260);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
             this.textBox2.TabIndex = 3;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(137, 313);
+            this.textBox3.BackColor = System.Drawing.SystemColors.Highlight;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox3.Location = new System.Drawing.Point(178, 313);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.Size = new System.Drawing.Size(100, 26);
             this.textBox3.TabIndex = 4;
             // 
             // textBox4
             // 
+            this.textBox4.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.textBox4.Location = new System.Drawing.Point(178, 360);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.Size = new System.Drawing.Size(100, 26);
             this.textBox4.TabIndex = 5;
             // 
             // UserNamelbl
             // 
             this.UserNamelbl.AutoSize = true;
+            this.UserNamelbl.BackColor = System.Drawing.Color.Transparent;
             this.UserNamelbl.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.UserNamelbl.Location = new System.Drawing.Point(28, 207);
+            this.UserNamelbl.Location = new System.Drawing.Point(45, 208);
             this.UserNamelbl.Name = "UserNamelbl";
             this.UserNamelbl.Size = new System.Drawing.Size(103, 25);
             this.UserNamelbl.TabIndex = 6;
@@ -101,8 +113,9 @@
             // Emaillbl
             // 
             this.Emaillbl.AutoSize = true;
+            this.Emaillbl.BackColor = System.Drawing.Color.Transparent;
             this.Emaillbl.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.Emaillbl.Location = new System.Drawing.Point(28, 260);
+            this.Emaillbl.Location = new System.Drawing.Point(65, 261);
             this.Emaillbl.Name = "Emaillbl";
             this.Emaillbl.Size = new System.Drawing.Size(66, 25);
             this.Emaillbl.TabIndex = 10;
@@ -111,8 +124,9 @@
             // Passwordlbl
             // 
             this.Passwordlbl.AutoSize = true;
+            this.Passwordlbl.BackColor = System.Drawing.Color.Transparent;
             this.Passwordlbl.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.Passwordlbl.Location = new System.Drawing.Point(28, 307);
+            this.Passwordlbl.Location = new System.Drawing.Point(53, 312);
             this.Passwordlbl.Name = "Passwordlbl";
             this.Passwordlbl.Size = new System.Drawing.Size(95, 25);
             this.Passwordlbl.TabIndex = 11;
@@ -138,12 +152,25 @@
             this.SignUpButtonlbl.TabIndex = 13;
             this.SignUpButtonlbl.Text = "Sign Up";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::Stock_analysis_client.Properties.Resources.GoldenStock;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(474, 108);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(302, 231);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
             // Register
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Stock_analysis_client.Properties.Resources.BG;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.SignUpButtonlbl);
             this.Controls.Add(this.PasswordAgainlbl);
             this.Controls.Add(this.Passwordlbl);
@@ -155,8 +182,10 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.SingUplbl);
+            this.DoubleBuffered = true;
             this.Name = "Register";
             this.Text = "Register";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -175,5 +204,6 @@
         private System.Windows.Forms.Label Passwordlbl;
         private System.Windows.Forms.Label PasswordAgainlbl;
         private System.Windows.Forms.Label SignUpButtonlbl;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
