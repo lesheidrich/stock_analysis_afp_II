c9ca85ff5340c98fb58cf1558861b5b7230009fa
Debsz <szidebi@gmail.com>
2023-04-17 15:06:07 +0200

    client and register

--- Client/Stock_analysis_client/Stock_analysis_client/Login.cs
+++ Client/Stock_analysis_client/Stock_analysis_client/Login.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,7 +14,19 @@ namespace Stock_analysis_client
 {
     public partial class Login : Form
     {
-        public Login()
+        HttpClient client = new HttpClient();
+
+        private static Login instance;
+        public static Login GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Login();
+            }
+            return instance;
+        }
+
+        private Login()
         {
             InitializeComponent();
         }
@@ -25,12 +38,20 @@ namespace Stock_analysis_client
 
         private void Loginbt_Click(object sender, EventArgs e)
         {
+            RestClient cls = new RestClient("http://localhost:5000/api/users/login");
+            RestRequest request = new RestRequest();
+            request.AddParameter("username", Usernametb.Text);
+            request.AddParameter("pwd", Passwordtb.Text);
+
+            RestResponse res = cls.Get(request);
+            MessageBox.Show(res.ToString());
 
         }
 
         private void SingUpbt_Click(object sender, EventArgs e)
         {
-
+           new Register().Show();
+            Hide();
         }
 
         private void Usernametb_TextChanged(object sender, EventArgs e)
