db594d77124afd8a8348cd560f210d633a8ac0b8
Debsz <szidebi@gmail.com>
2023-04-24 14:36:40 +0200

    Login teljes

--- Client/Stock_analysis_client/Stock_analysis_client/Login.cs
+++ Client/Stock_analysis_client/Stock_analysis_client/Login.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Net.Http;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -31,37 +32,76 @@ namespace Stock_analysis_client
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        public void ClearInput()
         {
-
+            Usernametb.Text = "";
+            Passwordtb.Text = "";
         }
 
-        private void Loginbt_Click(object sender, EventArgs e)
+        private void SignIn()
         {
             RestClient cls = new RestClient("http://localhost:5000/api/users/login");
             RestRequest request = new RestRequest();
             request.AddParameter("username", Usernametb.Text);
             request.AddParameter("pwd", Passwordtb.Text);
 
-            RestResponse res = cls.Get(request);
-            MessageBox.Show(res.ToString());
+            // RestResponse res = cls.Get(request);
+            // MessageBox.Show(res.ToString());
 
+            try
+            {
+                RestResponse response = cls.Get(request);
+                if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                {
+                    MessageBox.Show(response.StatusDescription);
+                }
+                else
+                {
+                    Response res = cls.Deserialize<Response>(response).Data;
+                    if (res.Status != 0)
+                    {
+                        MessageBox.Show(res.Message);
+                    }
+                    else
+                    {
+                        User u = new User()
+                        {
+                            Username = Usernametb.Text,
+                            Password = Passwordtb.Text
+                        };
+
+                        new Main(this).Show();
+                        this.Hide();
+                        ClearInput();
+                    }
+                }
+            }
+            catch (DeserializationException)
+            {
+                MessageBox.Show("Deserialization error", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Unknown error", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Loginbt_Click(object sender, EventArgs e)
+        {
+            SignIn();
+        }
+        private void EnterPressed(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                SignIn();
+            }
         }
 
         private void SingUpbt_Click(object sender, EventArgs e)
         {
            new Register().Show();
             Hide();
-        }
-
-        private void Usernametb_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Passwordtb_TextChanged(object sender, EventArgs e)
-        {
-
         }
     }
 }
---
+++ Client/Stock_analysis_client/Stock_analysis_client/User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stock_analysis_client
+{
+    public class User
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+
+        private string username;
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+
+        private string password;
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        private int isAdmin;
+        public int IsAdmin
+        {
+            get { return isAdmin; }
+            set { isAdmin = value; }
+        }
+
+        private int apikey;
+
+        public int APIKey
+        {
+            get { return apikey; }
+            set { apikey = value; }
+        }
+
+    }
+}
